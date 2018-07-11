194473ec456bb9504eadc94c1da38242902b700d
Jonathan Hong <jonhong777@gmail.com>
2018-07-11 04:07:42 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  attr_reader :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end
+end
